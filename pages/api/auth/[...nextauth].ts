6abfda96030be9fe57007ad38e4b4e59216d7fbe
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-08 21:14:55 +0100

    Added a GitHub Provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github"
 
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
@@ -13,10 +14,14 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    })
     // ...add more providers here
   ],
   // jwt: {
-  //   signingKey: process.env.JWT_SECRET!
-  // }
+  //   signingKey: process.env.JWT_SECRET!,
+  // },
   adapter: PrismaAdapter(prisma)
 })
