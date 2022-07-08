489894958161c1310c260208ae4526f843be0d01
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-08 21:32:16 +0100

    Added a signed out option for the Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,39 @@
+import { useSession, signIn, signOut } from "next-auth/react"
 import { FiSettings } from 'react-icons/fi';
 
 export default function Navbar(): JSX.Element {
-    return (
-        <div className="flex">
-            <div className="w-fit px-1 flex-shrink-0">
-                <h1 className="text-2xl font-bold">Task Timer</h1>
-            </div>
-            <div className="w-full">
-                <div className="flex justify-end items-center">
-                    <div className="p-1">
-                        <FiSettings size={30} className="hover:cursor-pointer" />
+    const { data: session } = useSession();
+
+    if(session) {
+        return (
+            <div className="flex">
+                <div className="w-fit px-1 flex-shrink-0">
+                    <h1 className="text-2xl font-bold">Task Timer</h1>
+                </div>
+                <div className="w-full">
+                    <div className="flex justify-end items-center">
+                        <div className="p-1">
+                            <FiSettings size={30} className="hover:cursor-pointer" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="flex bg-gray-100">
+                <div className="w-fit px-1 flex-shrink-0">
+                    <h1 className="text-2xl font-bold">Task Timer</h1>
+                </div>
+                <div className="w-full">
+                    <div className="flex justify-end items-center">
+                        <div className="p-1">
+                        <button className="bg-gray-300 rounded-md px-1 mr-1.5" onClick={() => signIn()}>Sign in</button>
+                        <button className="border-gray-300 bg-transparent border border-solid rounded-md px-1" onClick={() => signIn()}>Sign up</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,11 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession } from "next-auth/react"
+import Navbar from "../components/Navbar";
 
 export default function Component() {
   const { data: session } = useSession();
 
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user!.email} <br />
-        <button className="bg-slate-300" onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button className="bg-slate-300" onClick={() => signIn()}>Sign in</button>
-    </>
+  return(
+    <Navbar />
   )
+  
 }
