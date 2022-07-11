f5fdb39e789b061d0e532b1cb2e35178f5e44633
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-11 19:37:09 +0100

    Added the task section

--- pages/task-timer.tsx
+++ pages/task-timer.tsx
@@ -7,7 +7,16 @@ export default function TaskTimer() {
     return (
         <div>
             <Navbar />
-            <div>Task Timer</div>
+            <div className="w-full h-full flex justify-center items-center">
+                <h1 className="text-3xl font-bold">Task Timer</h1>
+            </div>
+            <div className="flex justify-center">
+                <div className="flex justify-evenly">
+                    <p className="mr-2">Make the task timer</p>
+                    <p className="mr-2 bg-blue-400 text-white font-bold px-1 rounded-sm">00:30:10</p>
+                    <button className="bg-green-300 rounded-md px-1">Play</button>
+                </div>
+            </div>
         </div>
     )
 }
